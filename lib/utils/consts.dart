957e276c576e1ec1dcaddaaa80694f37ed10e4fd
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-11-28 01:54:50 +0500

    Added hardcoded Colors in consts

---
+++ lib/utils/consts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const korange = Color(0xFFF87B36);
+const kblack = Color(0xFF2A2F44);
+const kyellow = Color(0xFFF5BA61);
+const kpink = Color(0xFFF286BD);
+const kbgColor = Color(0xFFF5F5F5);
